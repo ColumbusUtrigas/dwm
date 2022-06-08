36d3819b9d49d4bd39665ee4e2d95b19693941fd
ColumbusUtrigas <columbusutrigas@gmail.com>
2022-06-08 19:40:43 +0400

    basic setup

--- config.h
+++ config.h
@@ -1,12 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = {
+	"JetBrainsMono Nerd Font:size=12",
+	//"monospace:size=12",
+	"FontAwesome 6 Free:size=12",
+	//"NotoColorEmoji:size=10"
+};
+static const char dmenufont[]       = "monospace:size=10:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -18,10 +25,12 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
-/* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *keyboardLayouts[] = { "us", "ru" };
 
-static const Rule rules[] = {
+/* tagging */
+static const char *tags[] = { "", "", "", "", "", "", "", "", "9" };
+
+static const Rule rules[] = {i
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
@@ -59,11 +68,19 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "konsole", NULL };
+static const char *browsercmd[] = { "firefox", NULL };
+static const char* screenshot[] = { "spectacle", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ControlMask,           XK_t,      spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,           XK_f,      spawn,          {.v = browsercmd } },
+	{ 0, XF86XK_AudioRaiseVolume,              spawn,          SHCMD("pamixer --allow-boost -i 5; pkill -RTMIN+10 dwmblocks") },
+	{ 0, XF86XK_AudioLowerVolume,              spawn,          SHCMD("pamixer --allow-boost -d 5; pkill -RTMIN+10 dwmblocks") },
+	{ 0, XF86XK_AudioMute,                     spawn,          SHCMD("pamixer -t; pkill -RTMIN+10 dwmblocks") },
+	{ 0, XK_Print,                             spawn,          {.v = screenshot } },
+	{ MODKEY,                       XK_Shift_L,spawn,          {.v }},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
