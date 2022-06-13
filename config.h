dc339f248a4191559a99df32a3db421efb6adae4
ColumbusUtrigas <columbusutrigas@gmail.com>
2022-06-13 15:04:05 +0400

    mozilla xinput2

--- config.h
+++ config.h
@@ -86,8 +86,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "konsole", NULL };
-static const char *browsercmd[] = { "firefox", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *browsercmd[] = { "MOZ_USE_XINPUT2=1", "firefox", NULL };
 static const char* screenshot[] = { "flameshot", "gui", NULL };
 
 static Key keys[] = {
