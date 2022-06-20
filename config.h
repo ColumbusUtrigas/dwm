8d5cba3798c3a465ad081eb37a48ec7c520aad7c
ColumbusUtrigas <columbusutrigas@gmail.com>
2022-06-21 03:34:28 +0400

    galculator floating

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:antialias=true:autohint=true", };
 static const char dmenufont[]       = "monospace:size=10:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -36,6 +36,7 @@ static const char *const autostart[] = {
 	"dwmblocks", NULL,
 	"setxkbmap", "-layout", "us,ru", NULL,
 	"bg-set-image", NULL,
+	"sh", "-c",  "CM_SELECTIONS=\"clipboard\" clipmenud", NULL,
 	NULL /* terminate */
 };
 
@@ -54,8 +55,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",       NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",    NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Galculator", NULL,       NULL,       0,            1,           -1 }
 };
 
 /* layout(s) */
@@ -87,8 +89,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *browsercmd[] = { "MOZ_USE_XINPUT2=1", "firefox", NULL };
+static const char *browsercmd[] = { "firefox", NULL };
 static const char* screenshot[] = { "flameshot", "gui", NULL };
+static const char* lockscreencmd[] = { "slock", NULL };
+static const char* clipboardcmd[] = { "clipmenu", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -100,6 +104,8 @@ static Key keys[] = {
 	{ 0, XF86XK_AudioMute,                     spawn,          SHCMD("pamixer -t; pkill -RTMIN+10 dwmblocks") },
 	{ 0, XK_Print,                             spawn,          {.v = screenshot } },
 	{ WINKEY,                       XK_space,  switchKeyboardLayout, {.i = +1 } },
+	{ WINKEY,                       XK_l,      spawn,          {.v = lockscreencmd } },
+	{ WINKEY,                       XK_v,      spawn,          {.v = clipboardcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
